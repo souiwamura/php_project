d80657892f8d81327c2e20898c73899bf1abbda8
souiwamura <sisi1981art@gmail.com>
2019-04-21 23:43:32 +0900

    レイアウト周りの修正

--- resources/views/posts/layout.blade.php
+++ resources/views/posts/layout.blade.php
@@ -2,13 +2,17 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <!-- 今は大丈夫だけどEdgeでデザインが崩れるみたいなので対応 -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- レスポンシブル対応 ピンチには非対応 -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  
     <title>Laravel BBS</title>
 
+    <!-- bootstrapフォルダ内では読み込めないため移動 変更対応容易にするためのローカル読み込み -->
     <link
         rel="stylesheet"
-        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-        crossorigin="anonymous">
+        href="{{ url('/') }}/css/bootstrap.min.css">
 </head>
 <body>
     <header class="navbar navbar-dark bg-dark">
